160990f54341c31bd540df6955b788c7dcc76d75
maciejWUT <mciek.sieradzki@gmail.com>
2022-10-25 01:20:54 +0200

    zad2

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -25,6 +25,17 @@ public:
     double getX() {return x;} 
     double getY() {return y;}
 
+
+    
+    friend Wektor2D operator+(Wektor2D vec_1, Wektor2D vec_2)
+    {
+        return Wektor2D(vec_1.getX() + vec_2.getX(), vec_1.getY() + vec_2.getY());
+    };
+
+    friend double operator*(Wektor2D vec_1, Wektor2D vec_2)
+    {
+        return vec_1.getX() * vec_2.getX() + vec_1.getY() * vec_2.getY();
+
     
 	// Tutaj napisz implementacje klasy Wektor2D
 };
