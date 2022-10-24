9f4fd79d4a5cd10c315722c0f65866b4a89161d4
maciejWUT <mciek.sieradzki@gmail.com>
2022-10-24 23:43:04 +0200

    test1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,11 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    float x;
+    float y;
+
+    float norm() const { return sqrt(x * x + y * y); };
+    void  print() const { std::cout << "x: " << x << " y: " << y << std::endl; };
+	
+	// Tutaj napisz implementacje klasy Wektor2D
 };
---
+++ main.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+    Vectror2D v;
+    v.x = 3;
+    v.y = 4;
+    v.print();
+    std::cout << "norm: " << v.norm() << std::endl;
+    return 0;
+}
